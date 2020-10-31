7c5b18487d506b64f9a04ef91175b34782eb1ade
shonansurvivors <shonansurvivors@gmail.com>
2020-10-31 22:25:37 +0900

    5-8 アプリケーションの一部機能を変更してデプロイする

--- resources/views/articles/card.blade.php
+++ resources/views/articles/card.blade.php
@@ -1,7 +1,7 @@
 <div class="card mt-3">
   <div class="card-body d-flex flex-row">
     <a href="{{ route('users.show', ['name' => $article->user->name]) }}" class="text-dark">
-      <i class="fas fa-user-circle fa-3x mr-1"></i>
+      <i class="fas fa-user-ninja fa-3x mr-1"></i>
     </a>
     <div>
       <div class="font-weight-bold">
